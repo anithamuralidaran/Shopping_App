1f625d90598e487c94ce3bb15bc734a2c22bf160
anithakothandapani <anithakothandapani@192.168.0.102>
2020-05-01 11:44:41 +0530

    Test

--- ATMProject/src/com/atm/ATM.java
+++ ATMProject/src/com/atm/ATM.java
@@ -4,5 +4,10 @@ public class ATM {
 	
 	public void deposit() {}
 	public void withdraw() {}
+	boolean isNegative() {return false;}
+	boolean isZeroBalance() {return false;}
+	boolean isInsufficientFund() {return false;}
+	
+	
 
 }
